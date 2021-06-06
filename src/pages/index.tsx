cdf6487f0ae8f959737166cee33d873de16bcf7d
Abdullah Mzaien <s201540830@kfupm.edu.sa>
2021-06-06 19:04:38 +0300

    updating the template example 🦹‍♂️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,11 @@ function Index({ name }: indexProps) {
   return (
     <p
       style={{
+        margin: 0,
+        width: "100%",
+        height: "80vh",
+        background: "#111",
+        color: "#fcfcfc",
         padding: "1.5rem",
         fontSize: "2rem",
         fontFamily: "monospace",
@@ -16,6 +21,6 @@ function Index({ name }: indexProps) {
   );
 }
 function Home() {
-  return <Index name="Abdullah"></Index>;
+  return <Index name="To the 🚀 🌕"></Index>;
 }
 export default Home;
