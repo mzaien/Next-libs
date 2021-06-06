05cc319a0408a63abda413c565c6bc634cfebf27
Abdullah Mzaien <s201540830@kfupm.edu.sa>
2021-06-06 19:05:10 +0300

    Updating the template example

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,11 @@ function Index({ name }) {
   return (
     <p
       style={{
+        margin: 0,
+        width: "100%",
+        height: "80vh",
+        background: "#111",
+        color: "#fcfcfc",
         padding: "1.5rem",
         fontSize: "2rem",
         fontFamily: "monospace",
@@ -12,6 +17,6 @@ function Index({ name }) {
   );
 }
 function Home() {
-  return <Index name="Abdullah"></Index>;
+  return <Index name="To the 🚀 🌕"></Index>;
 }
 export default Home;
